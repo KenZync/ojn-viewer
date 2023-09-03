5325ef0790ed2ed09d9fe77ac60caf8d966f907f
Ken Ken <kenzync@pm.me>
2023-09-03 20:25:01 +0700

    feat: sha256

--- server/api/dmjam/[id].js
+++ server/api/dmjam/[id].js
@@ -1,7 +1,12 @@
+import { createHash } from 'crypto';
 export default defineEventHandler(async(event) => {
     const id = getRouterParam(event, 'id')
 
-    const url = `https://github.com/KenZync/O2Jam-Database/raw/main/dmjam/o2ma${id}.ojn`;
+    const hash = createHash('sha256');
+    hash.update(`o2ma${id}.ojn`);
+    const hashDigest = hash.digest('hex').toUpperCase();
+
+    const url = `https://github.com/KenZync/O2Jam-Database/raw/main/dmjam/${hashDigest}`;
     console.log(url)
     const data = await $fetch(url);
     return data
