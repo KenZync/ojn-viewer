7076493b44b9f88d03582cde032144219de7f9f0
Ken Ken <kenzync@pm.me>
2023-09-10 19:31:12 +0700

    feat: test ffmpeg.wasm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
-export default {
+export default defineNuxtConfig({
   nitro: {
     preset: "netlify",
   },
-  modules: ["@nuxtjs/tailwindcss",'nuxt-headlessui','@hypernym/nuxt-anime'],
-};
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-headlessui",
+    "@hypernym/nuxt-anime",
+  ],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+    },
+    server: {
+      headers: {
+        "Cross-Origin-Embedder-Policy": "require-corp",
+        "Cross-Origin-Opener-Policy": "same-origin",
+      },
+    },
+  },
+});
