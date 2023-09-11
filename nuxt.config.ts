4067ac80d51455bda0a1d9632e761731be1159b2
Ken Ken <kenzync@pm.me>
2023-09-11 16:39:02 +0700

    Revert "feat: test ffmpeg.wasm"
    This reverts commit 7076493b44b9f88d03582cde032144219de7f9f0.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,6 @@
-export default defineNuxtConfig({
+export default {
   nitro: {
     preset: "netlify",
   },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "nuxt-headlessui",
-    "@hypernym/nuxt-anime",
-  ],
-  vite: {
-    optimizeDeps: {
-      exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
-    },
-    server: {
-      headers: {
-        "Cross-Origin-Embedder-Policy": "require-corp",
-        "Cross-Origin-Opener-Policy": "same-origin",
-      },
-    },
-  },
-});
+  modules: ["@nuxtjs/tailwindcss",'nuxt-headlessui','@hypernym/nuxt-anime'],
+};
